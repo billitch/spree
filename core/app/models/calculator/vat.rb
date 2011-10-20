4d5175b14d7ad73d6b99cff5c6c2a0038e4d4606
Thomas de Grivel <billitch@gmail.com>
2011-10-20 16:07:59 +0200

    detect and log corrupted vat calculators

--- core/app/models/calculator/vat.rb
+++ core/app/models/calculator/vat.rb
@@ -12,8 +12,11 @@ class Calculator::Vat < Calculator
   # list the vat rates for the default country
   def self.default_rates
     origin = Country.find(Spree::Config[:default_country_id])
-    calcs = Calculator::Vat.includes(:calculable => :zone).select {
-      |vat| vat.calculable.zone.country_list.include?(origin)
+    calcs = Calculator::Vat.includes(:calculable => :zone).select { |vat|
+      vat.calculable.zone.country_list.include?(origin)
+    rescue
+      logger.warn "corrupted VAT calculator: #{vat}"
+      nil
     }
     calcs.collect { |calc| calc.calculable }
   end
